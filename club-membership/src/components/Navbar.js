56952233aec8ae6abbec4d0dfea2e487d7f01b6e
Dominic Chen <dominic.chen630@gmail.com>
2024-04-02 17:23:09 -0400

    fixed navbar

--- club-membership/src/components/Navbar.js
+++ club-membership/src/components/Navbar.js
@@ -4,7 +4,7 @@ import Logo from '../assets/logo.png'
 export const Navbar = () => {
     return (
         <div className=' bg-gray-700 flex flex-row justify-between items-center w-full h-36 px-44'>
-            <Link to="/login">
+            <Link to="/">
                 <img src={Logo} alt="dancers" className=' h-28' />
             </Link>
             <h1 className=' text-white fancy text-6xl'>Welcome to our dance club!</h1>
