fe6a911843184a3e421dd1759b6ef15c66b5a1b3
AbiJ17 <abinashjegan17@gmail.com>
2024-04-02 15:54:00 -0400

    Created Registration and Welcome Screens

--- club-membership/src/App.js
+++ club-membership/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import RegistrationScreen from './pages/RegistrationScreen';
+import WelcomeScreen from './pages/WelcomeScreen';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <p>Welcome to the Rec Club!</p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Our club is dedicated to bringing together enthusiasts and professionals to share knowledge, network, and collaborate on exciting projects.</p>
+          <p>Whether you're looking to learn, share, or lead, there's a place for you here.</p>
+        </header>
+        {/* Update from Switch to Routes */}
+        <Routes>
+          <Route path="/register" element={<RegistrationScreen />} />
+          <Route path="/" element={<WelcomeScreen />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
