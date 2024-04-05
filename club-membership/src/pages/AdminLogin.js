b2e934a3ed38f41902aafc705a93fe7f6bb4b8d2
Dominic Chen <dominic.chen630@gmail.com>
2024-04-04 22:43:28 -0400

    added peiod to admin login

--- club-membership/src/pages/AdminLogin.js
+++ club-membership/src/pages/AdminLogin.js
@@ -37,7 +37,7 @@ export const AdminLogin = () => {
             <div className='flex flex-row h-full'>
                 <form className='flex flex-col box flex-2' onSubmit={handleSubmit}>
                     <div className='flex-1 flex flex-row items-end w-full justify-start pl-20'>
-                    <h1 className=' text-center font-semibold text-6xl bg-red-100 py-1 px-2 reddit-mono'>Admin</h1>
+                    <h1 className=' text-center font-semibold text-6xl bg-red-100 py-1 px-2 reddit-mono'>Admin.</h1>
                     </div>
                     <div className='flex-3 flex justify-center flex-col'>
                         <label>
